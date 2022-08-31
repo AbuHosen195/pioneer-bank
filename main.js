3231418d5549a2e1f66f5b567e722c54ace21b6f
AbuHosen <abu.hosen195@.com>
2022-08-31 21:52:07 +0600

    added deposite and balance

---
+++ main.js
@@ -0,0 +1,43 @@
+//login button even handler
+const EnterButton = document.getElementById('EnterButton');
+EnterButton.addEventListener('click', function(){
+    const welcomeArea = document.getElementById('Welcome-Area');
+    welcomeArea.style.display = 'none';
+    const transitionArea = document.getElementById('transition-area');
+    transitionArea.style.display = 'block'
+})
+// deposit button even handler
+
+const depositBtn = document.getElementById('addDeposit');
+depositBtn.addEventListener('click', function(){
+    const depositNumber = getInputNumber('depositAmount')
+    
+
+    
+
+    updateSpanText("currentDeposit", depositNumber);
+    updateSpanText("currentBalance", depositNumber);
+
+    document.getElementById('depositAmount').value = "";
+})
+
+// withdraw button even handler
+
+const withdrawButton = document.getElementById("addWithdraw");
+withdrawButton.addEventListener("click", function(){
+    const withdrawNumber = getInputNumber('withdrawAmount');
+    console.log(withdrawNumber);
+  
+})
+function getInputNumber (id){
+    const Amount = document.getElementById(id).value;
+    const amountNumber = parseFloat(Amount);
+    return amountNumber;
+}
+
+function updateSpanText(id, depositNumber) {
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const total = depositNumber + currentNumber;
+    document.getElementById(id).innerText = total;
+}
